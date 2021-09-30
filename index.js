cc1a2f00f28632b18270f99a33228c444eaf5667
Kenny <kluu006@ucr.edu>
2021-09-29 17:43:44 -0700

    simple chart with clov data

---
+++ index.js
@@ -0,0 +1,45 @@
+const Express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+require('dotenv').config();
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const DATABASE_NAME = "reddit"
+const http = require("http");
+const fs = require("fs");
+const path = require('path');
+var app = Express();
+var database, collection;
+
+
+const { JSDOM } = require( "jsdom" );
+const { window } = new JSDOM( "" );
+const $ = require( "jquery" )( window );
+
+
+
+app.get("/posts", (req, res) => {
+    collection.find({}).toArray((error, result) => {
+        if(error) {
+            return res.status(500).send(error);
+        }
+        res.json(result);
+    });
+});
+
+
+app.get('/',(req, res) =>{
+    res.sendFile(__dirname + '/index.html');
+    //res.json(result);
+});
+
+app.listen(3000,()=>{
+    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        database = client.db(DATABASE_NAME);
+        collection = database.collection("wallstreet-bets");
+        console.log("Connected to `" + DATABASE_NAME + "`!");
+    });
+});
+
